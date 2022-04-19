4f1c1654f14ed4b48683182de66301913c49f070
David <dacid994@gmail.com>
2022-04-19 23:58:26 +0300

    tnsSlideShowBetterForBegginers

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,36 @@
-$(document).ready(function(){
-    $('.carousel__inner').slick({
-        dots: false,
-        speed: 1200,
-        adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../img/icons/prevp.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../img/icons/next.svg"></button>',
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                  dots: true,
-                  arrows: false
-                }
-            }
-        ]
-            
-      });
+    var slider = tns({
+    container: '.carousel__inner',
+    controls: false,
+    nav: true,
+    navPosition: 'bottom',
+    responsive: {
+        /* 100: {
+            gutter: 30
+          }, доделать слайдшоу батоны для айфонов */
+        1200: {
+            items: 1,
+            slideBy: 'page',
+            autoplay: false,
+            nav: false,
+            controls: false,
+            autoWidth: false,
+            controlsText: [
+                '<img src="../img/icons/prev.svg">',
+                '<img src="../img/icons/next.svg">'
+            ]
+        }
+      },
+    
+    
   });
+
+
+
+
+  document.querySelector('.prev').onclick = function () {
+    slider.goTo('prev');
+  };
+  document.querySelector('.next').onclick = function () {
+    slider.goTo('next');
+  };
+
