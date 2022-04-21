e4afc96658e0d6891c1c24a00ea0df0f6b2b3452
David <dacid994@gmail.com>
2022-04-22 00:51:19 +0300

    catalogReadyJQ

---
+++ src/js/tabs.js
@@ -0,0 +1,19 @@
+
+      $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+      });
+      
+
+
+
+/* $(function() {
+    
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+      $(this)
+        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+        .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+    });
+    
+  }); */
